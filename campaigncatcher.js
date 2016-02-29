d300ef8c5f5b5ac54d4d1f3baad480319639eefa
Anthony DiSanti <anthony.disanti@gmail.com>
2016-02-29 02:46:32 -0500

    Adds campaigncatcher.js
    * Adds campaigncatcher.js to the repo
    * Updates sample.html to work with campaigncatcher.js

---
+++ campaigncatcher.js
@@ -0,0 +1,13 @@
+(function($) {
+
+function CampaignCatcher() {
+  this.params = {};
+
+}
+
+
+
+// Export campaign catcher into the global variable, cc
+window.cc = new CampaignCatcher;
+
+})(jQuery);
